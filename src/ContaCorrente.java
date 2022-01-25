b585aea37d44daa48ff586e70457dfa7a9e73733
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2022-01-24 23:35:57 -0300

    Commit inicial Abstração do desafio Banco Digital em Java e POO - Dio.me

---
+++ src/ContaCorrente.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+
+public class ContaCorrente {
+
+	public int agencia;
+	public int numero;
+	public BigDecimal saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+	
+	public void saldo() {
+		
+	}
+}
